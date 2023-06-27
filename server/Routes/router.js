e8bae5daf27ab1e9af72e499b539d5162f34e959
Krakov <93730442+KrakovBen@users.noreply.github.com>
2023-06-28 00:13:20 +0300

    Create Profile

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -2,12 +2,15 @@ const express = require ("express")
 const router = express.Router()
 const UserRoute = require('./User/UserRoute')
 
+const profileRestController = require('../profile/routes/profileRestController')
+
 // const cardsRestController = require("../cards/routes/cardsRestController")
 // const usersRestController = require("../users/routes/usersRestController")
 // const testRestController = require("../test/routes/testRestController")
 // const { handleError } = require("../utils/errorHandler")
 
 router.use("/user",UserRoute)
+router.use("/profile", profileRestController)
 // router.use("/cards",cardsRestController)
 // router.use("/test",testRestController)
 
--- server/profile/routes/profileRestController.js
+++ server/profile/routes/profileRestController.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const UserSchema = require("../../db/schemas/User")
 const { handleError } = require('../../utils/errorHandler')
 const { validateProfile } = require('../validations/userValidationService')
+const { createUpdateProfile } = require('../models/profileAccessDataService')
 
 router.post('/', async (req, res) => {
+	// Need to add AUTH!!
 	try {
 		let profile = req.body
         const { error } = validateProfile(profile)
         if (error) return handleError(res, 400, `Joi Error : ${error.details[0].message}`)
 
         profile = normalizedProfile(profile)
-
+		//Need to create normalizedProfile
+		profile = await createUpdateProfile(profile)
+		return res.send(profile).status(201)
 	} catch (error) {
 		return handleError(res, error.status, error.message)
 	}
